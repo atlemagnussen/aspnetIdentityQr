4fe638a9bd1730c5d60873d0a36dd039535f7747
Atle Magnussen <atlemagnussen@gmail.com>
2025-04-07 23:28:26 +0200

    styling central components

--- AspAuthLocal/wwwroot/js/site.js
+++ AspAuthLocal/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const systemDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+if (systemDark)
+    document.body.classList.add("dark")
+else
+    document.body.classList.add("light")
