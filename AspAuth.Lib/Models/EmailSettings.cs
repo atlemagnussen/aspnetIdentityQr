6245cb119e53766e11a5e03916b6401afe3d80a1
Atle Magnussen <atlemagnussen@gmail.com>
2025-04-14 23:24:11 +0200

    manage

--- AspAuth.Lib/Models/EmailSettings.cs
+++ AspAuth.Lib/Models/EmailSettings.cs
@@ -2,8 +2,8 @@ namespace AspAuth.Liv.Models;
 
 public record EmailSettings
 {
-    public string Server { get; init; }
+    public string Server { get; init; }  = string.Empty;
     public int Port { get; init; }
-    public string From { get; init; }
-    public string Password { get; init;}
+    public string From { get; init; } = string.Empty;
+    public string Password { get; init; }  = string.Empty;
 }
