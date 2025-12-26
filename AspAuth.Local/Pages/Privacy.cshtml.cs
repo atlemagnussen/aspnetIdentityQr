1f43b5ca6eb0466267fc08e16a1d16558ca1c3c3
Atle Magnussen <atlemagnussen@gmail.com>
2025-12-26 23:48:08 +0100

    claims

--- AspAuth.Local/Pages/Privacy.cshtml.cs
+++ AspAuth.Local/Pages/Privacy.cshtml.cs
@@ -9,10 +9,17 @@ namespace AspAuth.Local.Pages
         public PrivacyModel(ILogger<PrivacyModel> logger)
         {
             _logger = logger;
+            Claims = [];
         }
 
+        public Dictionary<string,string> Claims {get;set;}
         public void OnGet()
         {
+            var allClaims = User.Claims.ToList();
+            foreach (var claim in allClaims)
+            {
+                Claims.Add(claim.Type, claim.Value);
+            }
         }
     }
 
